3763539746f3d2c0f325213acf6cc3505601d017
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-02 13:25:12 -0700

    Added a second slider control

--- src/app/dashboard/dashboard-controller.js
+++ src/app/dashboard/dashboard-controller.js
@@ -3,4 +3,6 @@
 angular.module('ang09')
     .controller('DashboardCtrl', function () {
         var main = this;
+
+
     });
--- src/app/main/main-controller.js
+++ src/app/main/main-controller.js
@@ -3,7 +3,6 @@
 angular.module('ang09')
     .controller('MainCtrl', function ($scope) {
         var main = this;
-
     })
 
 console.log("end of main-controller");
