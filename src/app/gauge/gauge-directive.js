5c6f415c94b2c733d3d2e69193a4a66bf4f29dac
JeffRisberg <jrisberg@aol.com>
2016-04-03 18:51:02 -0700

    changed labelling to use tspan for proper left-right alignment

--- src/app/gauge/gauge-directive.js
+++ src/app/gauge/gauge-directive.js
@@ -39,7 +39,7 @@ angular.module('ang09')
                         var arcInnerFrac = metric.expected;
                         var arcOuterFrac = metric.current;
                         var progressFrac = metric.current / metric.expected;
-                        var progressLabel = (100 * metric.current).toFixed() + "%";
+                        var progressLabel = (100 * metric.current).toFixed() + "";
                         var outerColor = "arc_outer_norm";
 
                         if (progressFrac < 0.25) {
@@ -84,22 +84,27 @@ angular.module('ang09')
                                 .innerRadius(0)
                                 .outerRadius(43)
                                 .startAngle(0)
-                                .endAngle(2 * Math.PI)
+                                .endAngle(2 * Math.PI);
 
                             svg.append("path")
                                 .attr("class", "circle_center")
                                 .attr("d", circle_center);
 
-                            svg.append("text")
+                            var upperText = svg.append("text")
                                 .style("text-anchor", "middle")
-                                .attr("font-size", "20px")
-                                .text(progressLabel)
-                                .attr("transform", "translate(0,-2)");
+                                .attr("transform", "translate(0,-0)");
+
+                            upperText.append("tspan")
+                                .attr("font-size", "22px")
+                                .text(progressLabel);
+
+                            upperText.append("tspan")
+                                .attr("font-size", "14px")
+                                .text("%");
 
                             svg.append("text")
                                 .style("text-anchor", "middle")
                                 .attr("font-size", "12px")
-                                .attr("font-style", "oblique")
                                 .text("Progress")
                                 .attr("transform", "translate(0,15)");
                         }
