826571a8c358b8d31b346f4824f7ed5d550a93fa
Brandon Risberg <brandon.risberg@gmail.com>
2016-04-17 13:14:55 -0700

    comments

--- src/app/slider/slider-directive.js
+++ src/app/slider/slider-directive.js
@@ -8,9 +8,11 @@ angular.module('ang09')
             templateUrl: 'app/slider/slider.tmpl.html',
 
             link: function ($scope, $element, $attrs) {
+                // temporary strings bound to the results of the range inputs
                 $scope.actualStr  = $scope.metric.actual;
                 $scope.expectedStr = $scope.metric.expected;
 
+                // update the slider results back to the metrics with an explicit type conversion
                 $scope.$watch("actualStr", function () {
                     $scope.metric.actual = parseFloat($scope.actualStr);
                 });
